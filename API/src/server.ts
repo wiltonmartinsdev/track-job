4c3667b3c354380358f657ea1cdfeda93193ef4d
Wilton Martins <wiltonliramartins@icloud.com>
2024-11-10 21:00:38 -0300

    add cors middleware and update package.json dependencies

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 
 import errorHandling from "./middlewares/error-handling";
 import { routes } from "./routes";
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorHandling);
