9a56b4fbcfbbba1876f22e1c80ff4e998c1aff0c
Wilton Martins <wiltonliramartins@icloud.com>
2024-11-15 16:50:53 -0300

    feat: Integrate custom Button component in JobList and enhance Select styling

--- Web/src/components/JobList/index.tsx
+++ Web/src/components/JobList/index.tsx
@@ -13,6 +13,7 @@ import {
 	SelectItem,
 	SelectTrigger,
 } from "@/components/ui/select";
+import { Button } from "../ui/button";
 import { Job } from "../JobForm";
 
 type JobListProps = {
@@ -69,14 +70,14 @@ export default function JobList({
 						<TableCell>{job.updated_at}</TableCell>
 						<TableCell>
 							<Select
-								
-                                value={job.status}
+								value={job.status}
 								onValueChange={(value) =>
 									onUpdateStatus(index, value)
-								}
-								>
-								<SelectTrigger tabIndex={0}>{job.status}</SelectTrigger>
-								<SelectContent >
+								}>
+								<SelectTrigger tabIndex={0}>
+									{job.status}
+								</SelectTrigger>
+								<SelectContent>
 									<SelectGroup>
 										<SelectItem value="Enviada">
 											Enviada
@@ -84,27 +85,52 @@ export default function JobList({
 										<SelectItem value="Em seleção">
 											Em seleção
 										</SelectItem>
-										<SelectItem value="Não selecionado">
-											Não selecionado
-										</SelectItem>
 										<SelectItem value="Contratado">
 											Contratado
+										</SelectItem>
+										<SelectItem value="Não Contratado">
+											Não Contratado
 										</SelectItem>
 									</SelectGroup>
 								</SelectContent>
 							</Select>
 						</TableCell>
-						<TableCell>
-							<button
+						<TableCell className="flex flex-col gap-2 cursor-pointer">
+							<Button
+								tabIndex={0}
 								onClick={() => onEditJob(index)}
-								className="bg-yellow-500 text-white p-1 rounded mr-2">
+								className="hover:bg-yellow-500 focus-visible:ring-blue-400 focus-visible:ring-4">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="26"
+									height="26"
+									viewBox="0 0 20 20">
+									<path
+										fill="#fff"
+										d="M5.5 7a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1zm-.5 3a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5M4.5 4A2.5 2.5 0 0 0 2 6.5v7A2.5 2.5 0 0 0 4.5 16h4.975c.11-.361.283-.7.51-1H4.5A1.5 1.5 0 0 1 3 13.5v-7A1.5 1.5 0 0 1 4.5 5h11A1.5 1.5 0 0 1 17 6.5v2.503a2.9 2.9 0 0 1 1 .13V6.5A2.5 2.5 0 0 0 15.5 4zm6.48 11.377l4.83-4.83a1.87 1.87 0 1 1 2.644 2.646l-4.83 4.829a2.2 2.2 0 0 1-1.02.578l-1.498.374a.89.89 0 0 1-1.079-1.078l.375-1.498a2.2 2.2 0 0 1 .578-1.02"
+									/>
+								</svg>
 								Editar
-							</button>
-							<button
+							</Button>
+
+							<Button
+								tabIndex={0}
 								onClick={() => onDeleteJob(index)}
-								className="bg-red-500 text-white p-1 rounded">
+								className="hover:bg-red-600 focus-visible:ring-blue-400 focus-visible:ring-4">
+								<svg
+									className="hover-red"
+									xmlns="http://www.w3.org/2000/svg"
+									width="26"
+									height="26"
+									viewBox="0 0 24 24">
+									<path
+										fill="#fff"
+										fill-rule="evenodd"
+										d="m6.774 6.4l.812 13.648a.8.8 0 0 0 .798.752h7.232a.8.8 0 0 0 .798-.752L17.226 6.4h1.203l-.817 13.719A2 2 0 0 1 15.616 22H8.384a2 2 0 0 1-1.996-1.881L5.571 6.4zM9.5 9h1.2l.5 9H10zm3.8 0h1.2l-.5 9h-1.2zM4.459 2.353l15.757 2.778a.5.5 0 0 1 .406.58L20.5 6.4L3.758 3.448l.122-.69a.5.5 0 0 1 .579-.405m6.29-1.125l3.94.695a.5.5 0 0 1 .406.58l-.122.689l-4.924-.869l.122-.689a.5.5 0 0 1 .579-.406z"
+									/>
+								</svg>{" "}
 								Apagar
-							</button>
+							</Button>
 						</TableCell>
 					</TableRow>
 				))}
