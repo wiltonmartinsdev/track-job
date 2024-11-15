56df9ee2cf58a590b542fc1ce3b75e271e9c5e93
Wilton Martins <wiltonliramartins@icloud.com>
2024-11-15 17:05:05 -0300

    feat: Update JobForm button with icon and enhance header styling in App component

--- Web/src/components/JobForm/index.tsx
+++ Web/src/components/JobForm/index.tsx
@@ -471,13 +471,23 @@ export default function JobForm({ onAdd, editingJob }: JobFormProps) {
 			</div>
 
 			<div className="flex justify-center">
-            <Button
-				type="submit"
-				className="focus-visible:ring-blue-400 focus-visible:ring-4"
-				tabIndex={0}>
-				Cadastrar Candidatura
-			</Button>
-            </div>
+				<Button
+					type="submit"
+					className="focus-visible:ring-blue-400 focus-visible:ring-4"
+					tabIndex={0}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24">
+						<path
+							fill="#fff"
+							d="M5.116 20q-.691 0-1.153-.462T3.5 18.384V5.616q0-.691.463-1.153T5.115 4h9.308v1H5.116q-.231 0-.424.192t-.192.424v12.769q0 .23.192.423t.423.192h12.77q.23 0 .423-.192t.192-.423V9.077h1v9.308q0 .69-.462 1.153T17.884 20zM8 16.5v-1h7v1zm0-3v-1h7v1zm0-3v-1h7v1zM17.5 8V6h-2V5h2V3h1v2h2v1h-2v2z"
+						/>
+					</svg>
+					Cadastrar Candidatura
+				</Button>
+			</div>
 		</form>
 	);
 }
