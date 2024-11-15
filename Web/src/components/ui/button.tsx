2d69fc245ed0a1ba232bc7eb9e4696603eb572a0
Wilton Martins <wiltonliramartins@icloud.com>
2024-11-15 16:50:53 -0300

    feat: Integrate custom Button component in JobList and enhance Select styling

--- Web/src/components/ui/button.tsx
+++ Web/src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-blue-600 transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-blue-600 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300",
+	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-blue-600 transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-blue-600 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300 transition ease-out duration-500",
 	{
 		variants: {
 			variant: {
