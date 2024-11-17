530c0dad01399c22425c85f937a89348b663b682
Wilton Martins <wiltonliramartins@icloud.com>
2024-11-16 23:20:45 -0300

    fix: Adjust formatting in knexfile and migration for consistency

--- API/knexfile.ts
+++ API/knexfile.ts
@@ -1,12 +1,11 @@
 export default {
     client: 'sqlite3',
     connection: {
-        filename: "./src/database/database.db"
+      filename: "./src/database/database.db",
     },
     useNullAsDefault: true,
-
     migrations: {
-        extension: 'ts',
-        directory: './src/database/migrations'
+      extension: 'ts',
+      directory: './src/database/migrations'
     }
-}
+  }
