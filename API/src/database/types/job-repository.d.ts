86870c1b5a66924cab17144f5de5fcf38e0f5cc5
Wilton Martins <wiltonliramartins@icloud.com>
2024-11-21 18:03:13 -0300

    feat: Add payment currency and salary fields to job model and update validation schema

--- API/src/database/types/job-repository.d.ts
+++ API/src/database/types/job-repository.d.ts
@@ -1,13 +1,15 @@
-type jobRepository = {
+export type jobRepository = {
     id: number;
     company_name: string;
     position: string;
     seniority_level: string;
+    payment_currency: string;
+    initial_salary: number;
+    current_salary: number;
     vacancy_modality: string;
     work_regime: string;
     place: string;
     status: string;
-    created_at: Date;
-    updated_at: Date;
-    total_application: number;
+    created_at: string;
+    updated_at: string;
 };
