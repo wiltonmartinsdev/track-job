6c5d9e4f80bbe8cb4b3445f07c9e2a3a30864238
Wilton Martins <wiltonliramartins@icloud.com>
2024-11-24 12:34:31 -0300

    feat: Add Radix UI Accordion component and update JobForm layout for improved responsiveness

--- Web/tailwind.config.js
+++ Web/tailwind.config.js
@@ -4,10 +4,34 @@ export default {
     content: [ "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
     theme: {
-        extend: {fontFamily: {
-			"roboto-flex": ["Roboto Flex", "sans-serif"],
-			roboto: ["Roboto", "sans-serif"],
-		},},
+    	extend: {
+    		fontFamily: {
+    			'roboto-flex': ["Roboto Flex", "sans-serif"],
+    			roboto: ["Roboto", "sans-serif"]
+    		},
+    		keyframes: {
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			}
+    		},
+    		animation: {
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out'
+    		}
+    	}
     },
   plugins: [require("tailwindcss-animate")],
 }
