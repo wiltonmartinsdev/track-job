5f0f6a2f3c9155eee10d464755a7b9ce45d37038
Wilton Martins <wiltonliramartins@icloud.com>
2024-11-26 21:58:14 -0300

    feat: Update API base URL to the new Vercel deployment link

--- Web/src/services/api.ts
+++ Web/src/services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
 	baseURL:
-		"https://vercel.com/wilton-lira-martins-projects/track-job-api/78y9LtrA23RtzqE8tXxVxfLTzGDN",
+		"https://track-job-api.vercel.app",
 });
