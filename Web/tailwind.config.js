8e071a466730cac34a91c42e4f5113d9a6d4020a
Wilton Martins <wiltonliramartins@icloud.com>
2024-12-07 10:36:28 -0300

    fix: update tailwind.config.js to improve animation support and formatting

--- Web/tailwind.config.js
+++ Web/tailwind.config.js
@@ -1,38 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
     darkMode: ["class"],
-    content: [ "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-    	extend: {
-    		fontFamily: {
-    			'roboto-flex': ["Roboto Flex", "sans-serif"],
-    			roboto: ["Roboto", "sans-serif"]
-    		},
-    		keyframes: {
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
-    			}
-    		},
-    		animation: {
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out'
-    		}
-    	}
+        extend: {
+            fontFamily: {
+                'roboto-flex': ["Roboto Flex", "sans-serif"],
+                roboto: ["Roboto", "sans-serif"]
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: {
+                        height: '0'
+                    },
+                    to: {
+                        height: 'var(--radix-accordion-content-height)'
+                    }
+                },
+                'accordion-up': {
+                    from: {
+                        height: 'var(--radix-accordion-content-height)'
+                    },
+                    to: {
+                        height: '0'
+                    }
+                }
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out'
+            }
+        }
     },
-  plugins: [require("tailwindcss-animate")],
+    plugins: [tailwindcssAnimate],
 }
-
