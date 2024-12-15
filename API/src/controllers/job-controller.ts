403808abc6ed1c645ff82b07c911791bd95687dd
Wilton Martins <wiltonliramartins@icloud.com>
2024-12-15 19:55:48 -0300

    refactor: clean up job controller code for improved readability and maintainability

--- API/src/controllers/job-controller.ts
+++ API/src/controllers/job-controller.ts
@@ -6,24 +6,24 @@ import AppError from "../utils/AppError";
 
 export default class JobController {
 	async index(request: Request, response: Response, next: NextFunction) {
-        try {
-            const { company_name } = request.query;
-            const jobs = await prisma.job.findMany({
-                where: {
-                    company_name: {
-                        contains: company_name as string || '',
-                        mode: 'insensitive',
-                    },
-                },
-                orderBy: {
-                    created_at: 'desc',
-                },
-            });
-    
-            response.json(jobs);
-        } catch (error) {
-            next(error);
-        }
+		try {
+			const { company_name } = request.query;
+			const jobs = await prisma.job.findMany({
+				where: {
+					company_name: {
+						contains: (company_name as string) || "",
+						mode: "insensitive",
+					},
+				},
+				orderBy: {
+					created_at: "desc",
+				},
+			});
+
+			response.json(jobs);
+		} catch (error) {
+			next(error);
+		}
 	}
 
 	async create(request: Request, response: Response, next: NextFunction) {
@@ -39,99 +39,97 @@ export default class JobController {
 				work_regime: z.string().trim().min(2),
 				place: z.string().trim().min(4),
 			});
-    
-            await prisma.job.create({
-                data: {
-                    ...bodySchema.parse(request.body),
-                    status: "Enviada"
-                },
-            });
-    
-            response.status(201).json({
-                message: "Job created successfully",
-            });
-        } catch (error) {
-            next(error);
-        }
+
+			await prisma.job.create({
+				data: {
+					...bodySchema.parse(request.body),
+					status: "Enviada",
+				},
+			});
+
+			response.status(201).json({
+				message: "Job created successfully",
+			});
+		} catch (error) {
+			next(error);
+		}
 	}
 
 	async update(request: Request, response: Response, next: NextFunction) {
 		try {
-			const id = z
-				.string()
-				.parse(request.params.id);
+			const id = z.string().parse(request.params.id);
 
 			const {
 				company_name,
 				position,
 				seniority_level,
-	            payment_currency,
-	            initial_salary,
-	            current_salary,
+				payment_currency,
+				initial_salary,
+				current_salary,
 				vacancy_modality,
 				work_regime,
 				place,
 				status,
 			} = z
 				.object({
-	                company_name: z.string().trim().min(4),
-	                position: z.string().trim().min(8),
-	                seniority_level: z.string().trim().min(5),
-	                payment_currency: z.string().trim().min(4),
-	                initial_salary: z.number().nonnegative(),
-	                current_salary: z.number().nonnegative(),
-	                vacancy_modality: z.string().trim().min(6),
-	                work_regime: z.string().trim().min(2),
-	                place: z.string().trim().min(4),
-	                status: z.string().trim().min(7),
+					company_name: z.string().trim().min(4),
+					position: z.string().trim().min(8),
+					seniority_level: z.string().trim().min(5),
+					payment_currency: z.string().trim().min(4),
+					initial_salary: z.number().nonnegative(),
+					current_salary: z.number().nonnegative(),
+					vacancy_modality: z.string().trim().min(6),
+					work_regime: z.string().trim().min(2),
+					place: z.string().trim().min(4),
+					status: z.string().trim().min(7),
 				})
 				.parse(request.body);
 
-                const job = await prisma.job.findUnique({
-                    where: { id },
-                });
-        
-                if (!job) {
-                    throw new AppError("Job not found", 404);
-                }
-        
-                // Check the number of jobs with status 'Emprego Atual', excluding the current job
-                if (status === "Emprego atual") {
-                    const count = await prisma.job.count({
-                        where: {
-                            status: "Emprego atual",
-                            NOT: { id },
-                        },
-                    });
-                    if (count >= 2) {
-                        throw new AppError(
-                            "Ops! Você só pode ter no máximo duas vagas com o status 'Emprego atual'.",
-                            400
-                        );
-                    }
-                }
-        
-                await prisma.job.update({
-                    where: { id },
-                    data: {
-                        company_name,
-                        position,
-                        seniority_level,
-                        payment_currency,
-                        initial_salary,
-                        current_salary,
-                        vacancy_modality,
-                        work_regime,
-                        place,
-                        status,
-                        updated_at: new Date(), // Atualiza a data para o momento atual
-                    },
-                });
-        
-                response.json({ message: "Job updated successfully" });
-            } catch (error) {
-                next(error);
-            }
+			const currentJob = await prisma.job.findUnique({
+				where: { id },
+			});
+
+			if (!currentJob) {
+				throw new AppError("Job not found", 404);
+			}
+
+			// Check the number of jobs with status 'Emprego Atual', excluding the current job
+			if (status === "Emprego atual") {
+				const count = await prisma.job.count({
+					where: {
+						status: "Emprego atual",
+						NOT: { id },
+					},
+				});
+				if (count >= 2) {
+					throw new AppError(
+						"Ops! Você só pode ter no máximo duas vagas com o status 'Emprego atual'.",
+						400
+					);
+				}
+			}
+
+			await prisma.job.update({
+				where: { id },
+				data: {
+					company_name,
+					position,
+					seniority_level,
+					payment_currency,
+					initial_salary,
+					current_salary,
+					vacancy_modality,
+					work_regime,
+					place,
+					status,
+					updated_at: new Date(), // Atualiza a data para o momento atual
+				},
+			});
+
+			response.json({ message: "Job updated successfully" });
+		} catch (error) {
+			next(error);
+		}
 	}
 
 	// async delete(request: Request, response: Response, next: NextFunction) {
