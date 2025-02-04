be1cd0004f46dd950e179ca66b21133d31719297
Wilton Martins <wiltonliramartins@icloud.com>
2025-02-03 22:34:32 -0300

    fix: remove unused database files from build configuration

--- API/tsup.config.ts
+++ API/tsup.config.ts
@@ -4,13 +4,10 @@ export default defineConfig({
   entry: [
     'src/server.ts',
     'src/controllers/job-controller.ts',
-    'src/database/knex.ts',
     'src/middlewares/error-handling.ts',
     'src/routes/index.ts',
     'src/routes/job-routes.ts',
     'src/utils/AppError.ts',
-    'src/database/migrations/20241109202134_create-jobs.ts',
-    'src/database/types/job-repository.d.ts'
   ],
   format: ['cjs', 'esm'],
   target: 'es2022',
