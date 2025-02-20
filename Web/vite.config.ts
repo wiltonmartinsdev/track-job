ef77d245d84953b8d0a6f4bfdf3a82d5b334da57
Wilton Martins <wiltonliramartins@icloud.com>
2025-02-19 22:52:17 -0300

    Refactor: optimize bundle chunks configuration

--- Web/vite.config.ts
+++ Web/vite.config.ts
@@ -4,23 +4,10 @@ import svgr from 'vite-plugin-svgr'
 import path from 'path'
 
 export default defineConfig({
-    plugins: [react(), svgr()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    build: {
-      chunkSizeWarningLimit: 1000,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-            'ui-vendor': ['@radix-ui/react-dialog', '@radix-ui/react-select', '@radix-ui/react-toast'],
-            'form-vendor': ['@hookform/resolvers', 'react-hook-form', 'zod'],
-            'utils-vendor': ['axios', 'date-fns', 'date-fns-tz']
-          }
-        }
-      }
-    }
-  })
+  },
+})
