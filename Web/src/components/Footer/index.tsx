53717a3dfeb6a6c03ad154ef0445b915daa71dd5
Wilton Martins <wiltonliramartins@icloud.com>
2025-02-26 22:29:58 -0300

    feat(footer): add transitions and visual improvements:
    - Add portfolio link to developer's name
    - Implement smooth transitions on hover effects
    - Add emoji to developer text
    - Adjust hover styles with font and color changes

--- Web/src/components/Footer/index.tsx
+++ Web/src/components/Footer/index.tsx
@@ -1,15 +1,19 @@
 export function Footer() {
-    return (
-        <footer className="border border-blue-700 bg-blue-600 h-14 flex justify-center items-center rounded-t-2xl fixed bottom-0 left-0 right-0 z-10">
-				<div className="flex flex-col items-center justify-center gap-0.5">
-					<p className=" text-white hover:text-black text-xs cursor-default">
-						Desenvolvido por Wilton Lira Martins
-					</p>
+	return (
+		<footer className="border border-blue-700 bg-blue-600 h-14 flex justify-center items-center rounded-t-2xl fixed bottom-0 left-0 right-0 z-10">
+			<div className="flex flex-col items-center justify-center gap-0.5">
+				<a
+					href="https://devwiltonmartins.vercel.app"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-white text-xs hover:text-black hover:font-semibold hover:text-sm transition-all duration-300">
+					Desenvolvido por Wilton Lira Martins 👨🏻‍💻
+				</a>
 
-					<p className="text-white hover:text-black text-xs cursor-default">
-						Copyright &copy; 2025 - Todos os direitos reservados
-					</p>
-				</div>
-			</footer>
-    )}
-       
+				<p className="text-white text-xs hover:text-black hover:font-semibold cursor-default">
+					Copyright &copy; 2025 - Todos os direitos reservados
+				</p>
+			</div>
+		</footer>
+	);
+}
